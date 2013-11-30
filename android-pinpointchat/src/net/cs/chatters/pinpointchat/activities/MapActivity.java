5c32a6286bd544c546261f8b26df436e3e500da4
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2013-11-30 19:46:59 +0000

    Fix manifest file and add map functionality

--- android-pinpointchat/src/net/cs/chatters/pinpointchat/activities/MapActivity.java
+++ android-pinpointchat/src/net/cs/chatters/pinpointchat/activities/MapActivity.java
@@ -1,16 +1,44 @@
 package net.cs.chatters.pinpointchat.activities;
 
-import net.cs.chatters.pinpointchat.R;
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends Activity {
+import net.cs.chatters.pinpointchat.R;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_layout);
-    }
-}
+public class MapActivity extends FragmentActivity {
+  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+  static final LatLng KIEL = new LatLng(53.551, 9.993);
+  private GoogleMap map;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.map_layout);
+    map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+        .getMap();
+    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+        .title("Hamburg"));
+    Marker kiel = map.addMarker(new MarkerOptions()
+        .position(KIEL)
+        .title("Kiel")
+        .snippet("Kiel is cool")
+        .icon(BitmapDescriptorFactory
+            .fromResource(R.drawable.ic_launcher)));
+
+    // Move the camera instantly to hamburg with a zoom of 15.
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+    // Zoom in, animating the camera.
+    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+  }
+
+
+} 
